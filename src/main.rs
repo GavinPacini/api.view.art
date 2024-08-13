359fed362fc9f332d2ae4f3643047cc41531d504
loudsqueak <loudsqueak.media@gmail.com>
2024-08-13 15:54:00 -0700

    add ngrok url

--- src/main.rs
+++ src/main.rs
@@ -114,7 +114,7 @@ fn app(state: AppState) -> Router {
                     .allow_origin([
                         "http://localhost:5173".parse::<HeaderValue>().unwrap(),
                         "https://macaw-resolved-arguably.ngrok-free.app".parse::<HeaderValue>().unwrap(),
-                        "http://192.168.1.17:5173".parse::<HeaderValue>().unwrap(),
+                        "https://gentle-flea-officially.ngrok-free.app".parse::<HeaderValue>().unwrap(),
                         "https://view.art".parse::<HeaderValue>().unwrap(),
                     ])
                     .allow_methods([Method::GET, Method::POST, Method::OPTIONS])
